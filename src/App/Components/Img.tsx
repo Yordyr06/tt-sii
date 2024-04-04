c29a820d734a769f6502f599c6032d50233c69da
Yordy Almonte <yoalmonte.dev@outlook.com>
2024-04-04 16:11:54 -0400

    🐛 Image deploy bug fix

--- src/App/Components/Img.tsx
+++ src/App/Components/Img.tsx
@@ -1,7 +1,7 @@
 export const Img = (): JSX.Element => {
   return (
     <img
-      src="/assets/genericcreditcard.jpg"
+      src="/public/genericcreditcard.jpg"
       alt="Tarjeta de Credito"
       className="rounded-xl absolute top-24 w-80"
     />
