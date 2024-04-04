fe28bdbe15837837128209bf0df5415624942d85
Yordy Almonte <yoalmonte.dev@outlook.com>
2024-04-04 16:08:52 -0400

    🐛 Production bugs fixing

--- src/App/Components/Img.tsx
+++ src/App/Components/Img.tsx
@@ -1,7 +1,7 @@
 export const Img = (): JSX.Element => {
   return (
     <img
-      src="/src/Assets/genericcreditcard.jpg"
+      src="/assets/genericcreditcard.jpg"
       alt="Tarjeta de Credito"
       className="rounded-xl absolute top-24 w-80"
     />
